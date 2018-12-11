bcd9cf45ce53a138470d26654e7129ca8ff28066
brucedominguez <bruce@platformcrossfit.com.au>
2018-12-11 13:54:50 +0800

    update of 05 breakoutvariables main.tf

--- 06 breakingUpTheScript/main.tf
+++ 06 breakingUpTheScript/main.tf
@@ -11,10 +11,3 @@ resource "docker_container" "container_id" {
     external = "${var.ext_port}"
   }
 }
-#Output the IP Address of the Container
-output "IP Address" {
-  value = "${docker_container.container_id.ip_address}"
-}
-output "container_name" {
-  value = "${docker_container.container_id.name}"
-}
